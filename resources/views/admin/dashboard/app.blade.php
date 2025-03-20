0398e626f268adb077bca3b58158e972418c36b3
van khoa <vkhoa6642@gmail.com>
2025-03-20 22:31:33 +0700

    okok

---
+++ resources/views/admin/dashboard/app.blade.php
@@ -0,0 +1,14 @@
+@include('admin.layouts.libs_css')
+@include('admin.layouts.header')
+@include('admin.layouts.sidebar')
+
+
+@yield('content')
+
+
+@include('admin.layouts.footer')
+@include('admin.layouts.libs_js')
+
+
+
+
---
+++ resources/views/admin/layouts/libs_css.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en" data-layout="semibox" data-sidebar-visibility="show" data-topbar="light" data-sidebar="light" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable">
+
+
+<!-- Mirrored from themesbrand.com/velzon/html/corporate/dashboard-blog.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 14 Mar 2025 16:19:45 GMT -->
+<head>
+
+    <meta charset="utf-8" />
+    <title>Blog | Velzon - Admin & Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+    <!-- Layout config Js -->
+    <script src="assets/js/layout.js"></script>
+    <!-- Bootstrap Css -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" />
+    <!-- custom Css-->
+    <link href="assets/css/custom.min.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
